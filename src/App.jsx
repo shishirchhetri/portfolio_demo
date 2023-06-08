0a040239c04f09fc8f3bb298c214ae38e3ade7fd
spark <shishirchhetri45@gmail.com>
2023-06-08 08:40:49 +0545

    fixed protfolio

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ import web3 from "./assets/web3.png";
 import web4 from "./assets/web4.png";
 import web6 from "./assets/web6.png";
 import web5 from "./assets/web5.png";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode]= useState(false);
   return (
-    <>
-      <main className="bg-white px-10">
+    <div className={darkMode? "dark" : ""}>
+      <main className="bg-white px-10 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">spark</h1>
@@ -35,9 +37,9 @@ function App() {
           </nav>
 
           <div className="text-center p-10 ">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">Sparkle</h2>
-            <h3 className="text-2xl py-2">Developer and Designer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 ">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl ">Sparkle</h2>
+            <h3 className="text-2xl py-2 md:text-3xl">Developer and Designer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
               pariatur voluptatem eaque facilis, eius quam.
             </p>
@@ -46,7 +48,7 @@ function App() {
             <AiFillLinkedin />
             <AiFillTwitterCircle />
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden ">
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <img src={deved} alt="" />
           </div>
         </section>
@@ -57,7 +59,7 @@ function App() {
             <p className="text-md py-2 leading-8 text-gray-800">
               Lorem ipsum dolor sit, amet consectetur adipisicing elit.
               Voluptatem eum soluta pariatur! Ipsa, sit explicabo?{" "}
-              <span className="text-teal-500">agencies consulted for</span>{" "}
+              <span className="text-teal-500">agencies consulted for</span>
               <span className="text-teal-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
                 odio nesciunt earum laudantium! Est, sequi?
@@ -69,8 +71,8 @@ function App() {
             </p>
           </div>
 
-          <div>
-            <div className=" flex  flex-col text-center shadow-lg p-10 rounded-xl my-10 items-center">
+          <div className="md:flex gap-10">
+            <div className=" flex  flex-col text-center shadow-lg p-10 rounded-xl my-10 items-center ">
               <img width={100} height={100} src={design} alt="" />
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Beautifl designs
@@ -137,31 +139,31 @@ function App() {
               saepe.
             </p>
           </div>
-          <div>
-            <div>
+          <div className="flex gap-10 flex-wrap lg:flex py-4"> 
+            <div className="flex-1 basis-1/3">
               <img src={web1} alt="" />
             </div>
-            <div>
+            <div className="flex-1 basis-1/3">
               <img src={web2} alt="" />
             </div>
-            <div>
-              <img src={web3} alt="" />
+            <div className="flex-1 basis-1/3">
+              <img src={web3} alt="" className="object-center w-full h-full "/>
             </div>
-            <div>
-              <img src={web4} alt="" />
-            </div>
-
-            <div>
-              <img src={web5} alt="" />
+            <div className="flex-1 basis-1/3">
+              <img src={web4} alt="" className="object-cover" />
             </div>
 
-            <div>
+            <div className="flex-1 basis-1/3">
+              <img src={web5} alt="" className="object-center w-full h-full " />
+            </div>
+
+            <div className="flex-1 basis-1/3">
               <img src={web6} alt="" />
             </div>
           </div>
         </section>
       </main>
-    </>
+    </div>
   );
 }
 
